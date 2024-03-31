ca26e5f7ed95a91fbd88e604b14471d8aabea69c
Akmalzhan <toktasinovakmalzhan01@gmail.com>
2024-03-31 17:31:40 +0300

    add vercel and cors

--- index.js
+++ index.js
@@ -3,27 +3,32 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import authRouter from "./routers/auth.route.js"
-import userRouter from "./routers/user.route.js"
-
+import authRouter from "./routers/auth.route.js";
+import userRouter from "./routers/user.route.js";
 
 // constants
 const app = express();
 dotenv.config();
 
 // middlware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // router
-app.use("/api/auth", authRouter)
-app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 // mongoose connection
 async function start() {
   try {
     mongoose
-      .connect(`mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PASS}@cluster0.wjuhzc7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+      .connect(
+        `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PASS}@cluster0.wjuhzc7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      )
       .then((success) =>
         app.listen(
           process.env.PORT,
